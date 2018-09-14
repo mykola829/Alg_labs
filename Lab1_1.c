58cf7d949b952bd32881648a6a48cf0284b7b34b
mykola829 <40271583+mykola829@users.noreply.github.com>
2018-09-14 17:48:38 +0000

    Update Lab1_1.c

--- Lab1_1.c
+++ Lab1_1.c
@@ -5,14 +5,10 @@
 int main (void)
 {
 
-    float a = 1000.0;
-    float b = 0.0001;
+    float a1 = 1000.0;
+    float b1 = 0.0001;
 
-       int a1 = a*10000;
-       a1 = round(a1);
        
-       int b1 = b*10000;
-       b1 = round(b1);
        
 
     float r1 = pow(a1 + b1,3) - (pow(a1,3)+3*(pow(a1,2))*b1)/3*a1*(pow(b1,2)) + pow(b1,3);
